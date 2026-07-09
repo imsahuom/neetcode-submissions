2434a4f9a31004e51d5fc9e99ec5bc349768aa16
Om Sahu <mt1240764@iitd.ac.in>
2026-07-09 22:21:41 +0530

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> mp;
+        mp['I'] = 1; mp['C'] = 100; mp['V'] = 5; mp['X'] = 10; mp['L'] = 50; mp['D'] = 500; mp['M'] = 1000;
+        int n = s.size();
+        int ans = 0;
+        for(int i=0; i<n; i+=2){
+            if (i+1 < n && s[i] == 'I' && s[i+1] == 'V') ans += 4;
+            else if (i+1 < n && s[i] == 'I' && s[i+1] == 'X') ans += 9;
+            else if (i+1 < n && s[i] == 'X' && s[i+1] == 'L') ans += 40;
+            else if (i+1 < n && s[i] == 'X' && s[i+1] == 'C') ans += 90;
+            else if (i+1 < n && s[i] == 'C' && s[i+1] == 'D') ans += 400;
+            else if (i+1 < n && s[i] == 'C' && s[i+1] == 'M') ans += 900;
+            else{
+                ans+= mp[s[i]];
+                i--;
+            }
+        }
+        return ans;
+    }
+};
